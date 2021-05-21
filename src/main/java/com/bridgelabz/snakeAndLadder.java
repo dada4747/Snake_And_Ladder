b00ff64ccf6dc6058a1f3af62b5bfc74fdc9f1db
dada4747 <adsurerahul96@gmail.com>
2021-05-21 15:37:48 +0530

    ADDED:Code for check a Option NoPlay, Ladder, Snake

--- src/main/java/com/bridgelabz/snakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeAndLadder.java
@@ -1,15 +1,35 @@
 package com.bridgelabz;
+import java.lang.*;
 
 public class snakeAndLadder {
-    final static int NO_OF_PLAYER = 1;
-    final static int START_POSITION = 0;
-    public static void main(String[] args){
-        int position = 0;
-        int dieValue = (int) Math.floor(Math.random() * 6) + 1 ;
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
-        System.out.println("Welcome to Snake and Ladder Game");
-        System.out.println("Number of player is:" + NO_OF_PLAYER);
-        System.out.println("Player start position : " + position);
-        System.out.println("Player rolls the die and get :" + dieValue);
+    public static void main(String[] args){
+        int start = 0;
+        int roll = 0;
+        int dieValue = (int) Math.floor(Math.random() * 6) + 1 ;
+        System.out.println("Die value is :" + dieValue);
+
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        System.out.println("Option is :" + option);
+
+        switch (option){
+            case NO_PLAY:
+                roll = roll;
+                break;
+            case LADDER:
+                roll =+ dieValue;
+                break;
+            case SNAKE:
+                roll -= dieValue;
+                if (roll < 0){
+                    roll = 0;
+                }
+                break;
+        }
+        int position = (start + roll);
+        System.out.println("Current Position:" + position);
     }
 }
