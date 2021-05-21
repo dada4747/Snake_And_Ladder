deeac0db94f586f78acda817c18dc1ab61b1dbbc
dada4747 <adsurerahul96@gmail.com>
2021-05-21 13:20:21 +0530

    ADDED:Code for single player at 0 postion

---
+++ src/main/java/com/bridgelabz/snakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class snakeAndLadder {
+    final static int NO_OF_PLAYER = 1;
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake and Ladder Game");
+        int position = 0;
+        System.out.println("Player start position : " + position);
+        System.out.println("Number of player is:" + NO_OF_PLAYER);
+    }
+}
