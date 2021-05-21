e3c78968a4c80a1d3ee5be429b8e42270f3bc22d
dada4747 <adsurerahul96@gmail.com>
2021-05-21 14:56:56 +0530

    ADDED:Code for Roll die and get number

--- src/main/java/com/bridgelabz/snakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeAndLadder.java
@@ -2,10 +2,14 @@ package com.bridgelabz;
 
 public class snakeAndLadder {
     final static int NO_OF_PLAYER = 1;
+    final static int START_POSITION = 0;
     public static void main(String[] args){
-        System.out.println("Welcome to Snake and Ladder Game");
         int position = 0;
-        System.out.println("Player start position : " + position);
+        int dieValue = (int) Math.floor(Math.random() * 6) + 1 ;
+
+        System.out.println("Welcome to Snake and Ladder Game");
         System.out.println("Number of player is:" + NO_OF_PLAYER);
+        System.out.println("Player start position : " + position);
+        System.out.println("Player rolls the die and get :" + dieValue);
     }
 }
