c055a901f1f68bd3a88a6f4324f3c508926d2490
dada4747 <adsurerahul96@gmail.com>
2021-05-21 16:42:40 +0530

    ADDED:Code for repeat till winning at 100 postion

--- src/main/java/com/bridgelabz/snakeAndLadder.java
+++ src/main/java/com/bridgelabz/snakeAndLadder.java
@@ -5,31 +5,45 @@ public class snakeAndLadder {
     public static final int NO_PLAY = 0;
     public static final int LADDER = 1;
     public static final int SNAKE = 2;
-
+    public static final int WINNING_POSITION = 100;
     public static void main(String[] args){
-        int start = 0;
-        int roll = 0;
-        int dieValue = (int) Math.floor(Math.random() * 6) + 1 ;
-        System.out.println("Die value is :" + dieValue);
+        int position = 0;
+        int reach = 0;
+        while(reach < WINNING_POSITION) {
+            reach++;
+            int dieValue = (int) Math.floor(Math.random() * 6) + 1;
+            System.out.println("Die value is :" + dieValue);
 
-        int option = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println("Option is :" + option);
+            int option = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println("Option is :" + option);
 
-        switch (option){
-            case NO_PLAY:
-                roll = roll;
-                break;
-            case LADDER:
-                roll =+ dieValue;
-                break;
-            case SNAKE:
-                roll -= dieValue;
-                if (roll < 0){
-                    roll = 0;
-                }
-                break;
+            switch (option) {
+                case NO_PLAY:
+                    position += 0;
+                    break;
+                case LADDER:
+                    if((position + dieValue) < 100){
+                    position += dieValue;
+                    reach = position;
+                    }
+                    else
+                        position += dieValue;
+                        reach = position;
+
+                    break;
+                case SNAKE:
+                    if(position > 0){
+                        position -= dieValue;
+                        reach = position;
+                    }
+                    else
+                        position = 0;
+                        reach = position;
+
+                    break;
+            }
+            System.out.println("Reached:" + reach);
         }
-        int position = (start + roll);
-        System.out.println("Current Position:" + position);
+        System.out.println("Win:" +reach);
     }
 }
